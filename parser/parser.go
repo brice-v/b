d5d91f53c969a446b201974d3260e895671f52d4
Brice Vadnais <bricevadnais@yahoo.com>
2019-10-29 20:06:33 -0400

    Parser: Added scaffolding for parsing  var and val

---
+++ parser/parser.go
@@ -0,0 +1,114 @@
+package parser
+
+import (
+	"b/ast"
+	"b/lexer"
+	"b/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// Read two tokens so curToken and peekToken are both set
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.VAR:
+		return p.parseVarStatement()
+	case token.VAL:
+		return p.parseValStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseVarStatement() *ast.VarStatement {
+	stmt := &ast.VarStatement{Token: p.curToken}
+
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+
+}
+
+func (p *Parser) parseValStatement() *ast.ValStatement {
+	stmt := &ast.ValStatement{Token: p.curToken}
+
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+	
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		return false
+	}
+}
---
+++ parser/parser_test.go
@@ -0,0 +1,105 @@
+package parser
+
+import (
+	"b/ast"
+	"b/lexer"
+	"testing"
+)
+
+func TestVarStatements(t *testing.T) {
+	input := `
+var x = 5;
+var y = 10;
+var foobar = 5353;
+`
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d", len(program.Statements))
+	}
+
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testVarStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+
+func TestValStatements(t *testing.T) {
+	input := `
+	val x = 5;
+	val y = 10;
+	val foobar = 5353;
+	`
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d", len(program.Statements))
+	}
+
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testValStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+
+func testVarStatement(t *testing.T, s ast.Statement, name string) bool {
+	if s.TokenLiteral() != "var" {
+		t.Errorf("s.TokenLiteral not `let`. got=%q", s.TokenLiteral())
+		return false
+	}
+	varStmt, ok := s.(*ast.VarStatement)
+	if !ok {
+		t.Errorf("s not *ast.VarStatement. got=%T", s)
+		return false
+	}
+	if varStmt.Name.TokenLiteral() != name {
+		t.Errorf("s.Name not `%s`. got=%s", name, varStmt.Name)
+	}
+	return true
+}
+
+func testValStatement(t *testing.T, s ast.Statement, name string) bool {
+	if s.TokenLiteral() != "val" {
+		t.Errorf("s.TokenLiteral not `let`. got=%q", s.TokenLiteral())
+		return false
+	}
+	varStmt, ok := s.(*ast.ValStatement)
+	if !ok {
+		t.Errorf("s not *ast.ValStatement. got=%T", s)
+		return false
+	}
+	if varStmt.Name.TokenLiteral() != name {
+		t.Errorf("s.Name not `%s`. got=%s", name, varStmt.Name)
+	}
+	return true
+}
